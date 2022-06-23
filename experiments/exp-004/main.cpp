b8dcbe743958b424d5dedd1aa19934f5457d8811
Kamila <inchina_k@auca.kg>
2022-06-23 23:52:02 +0600

    exp-004 finished

---
+++ experiments/exp-004/main.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML app");
+    window.setVerticalSyncEnabled(true);
+
+    sf::Texture texture_2048;
+    texture_2048.loadFromFile("data/2048.png");
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture_2048);
+    sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
+    sprite.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+#ifdef AUCA_DEBUG
+    std::clog << "ok" << std::endl;
+#endif
+}
