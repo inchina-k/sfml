e8ee77c66e92fc32d3c441623dafc7532138770f
Kamila <inchina_k@auca.kg>
2022-06-29 20:40:09 +0600

    exp-006 finished && fixed

--- experiments/exp-006/main.cpp
+++ experiments/exp-006/main.cpp
@@ -5,18 +5,19 @@ using namespace std;
 
 int main()
 {
-    vector<vector<int>> puzzle = {{0, 0, 4, 2}, {0, 8, 0, 0}, {0, 64, 0, 32}, {0, 0, 2, 2}};
+    vector<vector<int>> puzzle = {{0, 0, 4, 2}, {0, 8, 0, 0}, {0, 64, 0, 32}, {0, 4, 2, 2}};
+
+    vector<vector<bool>> merged(puzzle.size(), vector<bool>(puzzle.size()));
 
     for (int step = 0; step < 4; step++)
     {
         bool is_changed = false;
-        vector<vector<bool>> merged(puzzle.size(), vector<bool>(puzzle.size()));
 
         for (int row = 0; row < 4; row++)
         {
             for (int col = 1; col < 4; col++)
             {
-                if (puzzle[row][col] == puzzle[row][col - 1] && puzzle[row][col] && !merged[row][col - 1])
+                if (puzzle[row][col] == puzzle[row][col - 1] && puzzle[row][col] && !merged[row][col])
                 {
                     puzzle[row][col - 1] *= 2;
                     puzzle[row][col] = 0;
